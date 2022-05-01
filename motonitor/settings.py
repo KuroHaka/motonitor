812daa4dc56ca0a8f602a5c0d7ea97eb1b12c192
KuroHaka <ceschen27@gmail.com>
2022-05-01 08:19:11 +0200

    final version

--- motonitor/settings.py
+++ motonitor/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'predictor'
+    'predictor',
+    'mainpage'
+
 ]
 
 MIDDLEWARE = [
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'motonitor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'mainpage/templates'), os.path.join(BASE_DIR, 'mainpage/static')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
